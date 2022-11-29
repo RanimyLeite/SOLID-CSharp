49ef3eb04677f74fc31c7dc98610006eb9efed5f
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-11-29 18:18:20 -0300

    Principio da Segregação de Interface - Problema

---
+++ SOLID-CSharp/4 - ISP/ISP - Problema/CadastroProdutos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._4___ISP.ISP___Problema
+{
+    public class CadastroProduto : ICadastro
+    {
+        public void ValidarDados()
+        {
+            // Validar valor
+        }
+
+        public void Salvar()
+        {
+            // Insert tabela Produto
+        }
+
+        //Um produto NÃo possui email, logo não precisa desse metodo
+        public void EnviarEmail()
+        {
+            // Produto não tem e-mail
+            // Qual deverá ser a implementação?
+        }
+    }
+}
---
+++ SOLID-CSharp/4 - ISP/ISP - Problema/CadastroUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._4___ISP.ISP___Problema
+{
+    public class CadastroUsuario : ICadastro
+    {
+        public void ValidarDados()
+        {
+            // Validar CPF, Email
+        }
+
+        public void Salvar()
+        {
+            // Insert na tabela Usuario
+        }
+
+        public void EnviarEmail()
+        {
+            // Enviar e-mail para o usuario
+        }
+    }
+}
