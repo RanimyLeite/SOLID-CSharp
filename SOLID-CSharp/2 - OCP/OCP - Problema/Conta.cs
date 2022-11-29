35f2980649421bd98f2b224ef1cea0c05797aba2
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-11-29 16:12:51 -0300

    Principio do Aberto/Fechado - Problema

---
+++ SOLID-CSharp/2 - OCP/OCP - Problema/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._2___OCP.OCP___Problema
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public decimal Saldo { get; set; }
+        public TipoConta TipoConta { get; set; }
+    }
+}
---
+++ SOLID-CSharp/2 - OCP/OCP - Problema/TipoConta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._2___OCP.OCP___Problema
+{
+    public enum TipoConta
+    {
+        Corrente,
+        Poupanca
+    }
+}
