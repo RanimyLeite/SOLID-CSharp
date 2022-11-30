5430da29b8c16cd9000d07beecc80c62c1655ca5
RanimyLeite <ranimy.leitecosta@gmail.com>
2022-11-29 21:01:48 -0300

    Principio da Inversão de Dependência - Problema

---
+++ SOLID-CSharp/5 - DIP/DIP - Problema/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._5___DIP.DIP___Problema
+{
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        //Aqui dependemos da implementação de EmailService e CPFService
+        public bool IsValid()
+        {
+            return EmailServices.IsValid(Email) && CPFServices.IsValid(CPF);
+        }
+    }
+}
---
+++ SOLID-CSharp/5 - DIP/DIP - Problema/UsuarioService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_CSharp._5___DIP.DIP___Problema
+{
+    public class UsuarioService
+    {
+        public string AdicionarUsuario(Usuario usuario)
+        {
+            if (!usuario.IsValid())
+                return "Dados inválidos";
+
+            //Dependencia do UsuarioRepository
+            var repo = new UsuarioRepository();
+            repo.AdicionarUsuario(usuario);
+
+            //Dependencia de EmailService
+            EmailServices.Enviar("csharp@solid.com", usuario.Email, "Bem Vindo", "Parabéns está Cadastrado");
+
+            return "Usuário cadastrado com sucesso";
+        }
+    }
+}
